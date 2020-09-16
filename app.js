edea2cd189302c4542ce8dfcf623cd9614020bf3
Gustavo Jejer <gjjejer@gmail.com>
2020-09-15 21:10:28 -0300

    Arreglando variables, proximo paso cargar de manera dinamica los productos restantes

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 //VARIABLES
-const btnCarro = document(".carro-btn"); //Boton del carro (lo despliega)
-const btnCerrarCarro = document(".cerrar-carro"); //Boton para cerrar el carro
-const btnLimpiarCarro = document(".limpiar-carro"); //boton para limpiar todos los items del carro
-const objetosCarro = document(".carro"); //objetos dentro del carro
-const ventanaCarro = document(".carro-superpuesto"); //Ventana principal del carro
-const contenedorCarro = document(".contenedor-carro"); //contenedor de los objetos agregados al carro
-const itemCarro = document(".carro-item"); //Items del carro
-const totalCarro = document(".carro-total"); //SPAN + H3 con el valor final de los objetos agregados al carro
-const productosCentrarDOM = document(".productos-centrar"); //contenedor para cada uno de los productos
+const btnCarro = $(".carro-btn"); //Boton del carro (lo despliega)
+const btnCerrarCarro = $(".cerrar-carro"); //Boton para cerrar el carro
+const btnLimpiarCarro = $(".limpiar-carro"); //boton para limpiar todos los items del carro
+const objetosCarro = $(".carro"); //objetos dentro del carro
+const ventanaCarro = $(".carro-superpuesto"); //Ventana principal del carro
+const contenedorCarro = $(".contenedor-carro"); //contenedor de los objetos agregados al carro
+const itemCarro = $(".carro-item"); //Items del carro
+const totalCarro = $(".carro-total"); //SPAN + H3 con el valor final de los objetos agregados al carro
+const productosCentrarDOM = $(".productos-centrar"); //contenedor para cada uno de los productos
 let carro = []; //objetos agregados al carro de compras
 
 //Obteniendo productos
-class productos {}
+class productos {
+  async obtenerProductos() {
+    try {
+      let resultado = await fetch("informacion.json");
+      let datos = await resultado.json();
+      let datosProductos = datos.items;
+    } catch (error) {}
+  }
+}
 
 //Mostrando productos
 class interfaz {}
@@ -19,9 +27,9 @@ class interfaz {}
 //Local storage
 class storage {}
 
-//Cuando se cargue todo el DOM...ARRANCAMOS! :D
 $(document).ready(() => {
-  console.log("Hola desde JQUERY ;)");
   const stg = new storage();
-  const productos = new productos();
+  const Productos = new productos();
+  //Obtener todos los productos
+  Productos.obtenerProductos().then((data) => console.log(data));
 });
